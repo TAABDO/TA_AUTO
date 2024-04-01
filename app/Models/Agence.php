60d43d5ce91e885a02fbdf2aa5aae956b9eeacaa
taouil abdo <abdotaouil03@gmail.com>
2024-04-01 10:45:20 +0000

    Refactor Agence and remove Annoncement model

--- app/Models/Agence.php
+++ app/Models/Agence.php
@@ -2,24 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Agence extends Model implements HasMedia
 {
-    use HasFactory, softDelete ,InteractsWithMedia;
+    use HasFactory, SoftDeletes ,InteractsWithMedia;
 
 
     protected $fillable=
     [
         'name',
-        'location',
+        'localisation',
         'type',
         'delete_at',
         'create_at',
         'update_at'
 
     ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
 }
