b5836c4b5c088ea4eb59f5ee213ef46048478ea1
taouil abdo <abdotaouil03@gmail.com>
2024-04-01 10:44:32 +0000

    Refactor UserController and add new methods

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::where('status', 'pending')->get();
+
+        return view('admin.admin', compact('users'));
+    }
+
+
+    public function create()
+    {
+        return view('admin.create');
+    }
+
+
+    public function Show(User $admin)
+    {
+
+        $roles = Role::All();
+        $statuses = User::distinct()->pluck('status')->toArray();
+
+        return view('Admin.users.update', compact('admin', 'roles', 'statuses'));
+    }
+
+    public function edit(User $admin)
+    {
+
+        return view('Admin.users.update', compact('admin'));
+    }
+
+    public function update(UserRequest $request, User $admin)
+    {
+        $data = $request->validated();
+        $admin->roles()->sync([$request->role_id]);
+        return redirect()->route('admin.index');
+    }
 }
