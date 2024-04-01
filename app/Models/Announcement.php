73995c0e8894fff815a6c9497a6cc008011e3bf9
taouil abdo <abdotaouil03@gmail.com>
2024-04-01 10:45:34 +0000

    Add Announcement and Brand models

---
+++ app/Models/Announcement.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Announcement extends Model implements HasMedia
+{
+    use HasFactory , SoftDeletes ,InteractsWithMedia;
+
+
+
+    protected $fillable=
+    [
+        'titel',
+        'description',
+        'price',
+        'situation',
+        'type',
+        'status',
+        'user_id',
+        'delete_at',
+        'create_at',
+        'update_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function cars()
+    {
+        return $this->hasMany(Car::class);
+    }
+}
