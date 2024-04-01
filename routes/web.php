f0f3e545501ed62ea8bbd0a2e8c716a9258a700f
taouil abdo <abdotaouil03@gmail.com>
2024-04-01 10:53:15 +0000

    Update routes/web.php with new controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Pages\BlogController;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Pages\ContactController;
-use App\Http\Controllers\Auth\regesteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,16 +20,31 @@ use App\Http\Controllers\Auth\regesteredUserController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// ============================ Admin =============================
+
+Route::resource('Admin',AdminController::class);
+
+Route::resource('brands',BrandController::class);
+
+
+
+// ======================================= Home ==============
+
 Route::get('Home',[HomeController::class,'car']);
-Route::get('admin',[AdminController::class,'admin'])->name('admin.admin');
+
+
+
+// Route::get('admin',[AdminController::class,'admin'])->name('admin.admin');
+// Route::get('admin',[AdminController::class,'index'])->name('admin.index');
+// Route::put('admin/{user}/update',[AdminController::class,'update'])->name('admin.update');
+
+
 Route::get('contact',[ContactController::class,'contact'])->name('contact.contact');
 Route::get('Blog',[BlogController::class,'Blog'])->name('Blog.Blog');
 
+// Route::get('users',[UserController::class,'index'])->name('users.index');
 
 
-
-// Route::resource('login',LoginController::class);
-// Route::resource('login',LoginController::class);
 
 
 Route::get('/', function () {
@@ -40,5 +55,6 @@ Route::get('car',[CarController::class,'index'])->name('car.index');
 
 Route::get('login',[AuthController::class,'login'])->name('login');
 Route::get('register',[AuthController::class,'register'])->name('register');
-
-
+Route::post('regesterUser',[AuthController::class,'regesterUser'])->name('regesterUser');
+Route::post('loginUser',[AuthController::class,'loginUser'])->name('loginUser');
+Route::get('logout',[AuthController::class,'logout'])->name('logout');
