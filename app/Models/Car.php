15273d5bf8e09572e38e10931e2b5508eba81bd3
taouil abdo <abdotaouil03@gmail.com>
2024-04-01 10:45:50 +0000

    Refactor Car and User models

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,35 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Car extends Model implements HasMedia
 {
-    use HasFactory, softDelete ,InteractsWithMedia;
+    use HasFactory, SoftDeletes ,InteractsWithMedia;
 
 
     protected $fillable = [
-        'name',
-        'price',
         'color',
         'model',
-        'Phone',
-        'status',
+        'seats',
+        'km',
+        'year',
+        'situation',
+        'transmission',
+        'fuel_type',
+        'engine_capacity',
+        'brand_id',
+        'annoncement_id',
+        'user_id',
         'delete_at',
         'create_at',
         'update_at'
 
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function announcement()
+    {
+        return $this->belongsTo(Announcement::class);
+    }
+
     public function brand()
     {
-        return $this->belongTo(Brand::class);
-    }
-    public function annoncement()
-    {
-        return $this->belongTo(Annoncement::class);
+        return $this->belongsTo(Brand::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class User extends Model implements HasMedia
 {
-    use HasFactory, softDelete ,InteractsWithMedia;
-
+    use HasFactory, InteractsWithMedia ,SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -21,13 +21,22 @@ class User extends Model implements HasMedia
         'status',
         'delete_at',
         'create_at',
-        'update_at'
+        'update_at',
 
     ];
 
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 
-        public function roles()
-        {
-            return $this->belongsToMany(Role::class);
-        }
+    public function agence()
+    {
+        return $this->belongsTo(Agence::class);
+    }
+
+    public function cars()
+    {
+        return $this->hasMany(Car::class);
+    }
 }
