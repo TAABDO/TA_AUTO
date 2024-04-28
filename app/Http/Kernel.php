3b97f8d365f0b4ecdb315b097118b930b836a2e7
taouil abdo <abdotaouil03@gmail.com>
2024-04-28 01:17:31 +0100

    Add middleware for Admin, Announcer, and Client roles

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'Admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'Announcer' => \App\Http\Middleware\AnnouncerMiddleware::class,
+        'Client' => \App\Http\Middleware\ClientMiddleware::class,
     ];
 }
